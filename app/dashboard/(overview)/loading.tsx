6c7213517e7d3a31cd31c78f4071a4f0d62321eb
joshheavy <warurijoshua21@gmail.com>
2025-02-21 09:38:19 +0300

    New files added

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,11 @@
+
+ import DashboardSkeleton from "@/app/ui/skeletons";
+
+ export default function Loading() {
+    return (
+        // <div className="flex justify-center items-center h-screen">
+        //     <p>Loading...</p>
+        // </div>
+        <DashboardSkeleton />
+    );
+}
